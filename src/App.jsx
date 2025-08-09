33b1b52a5cae31cee1574e5da6901e4c799d6b66
Ben Ellis <benellisqa@gmail.com>
2025-08-09 11:45:13 +0100

    Created navbar animations and styling

--- src/App.jsx
+++ src/App.jsx
@@ -3,10 +3,17 @@ import { Navbar } from "./components/Navbar";
 import { Hero } from "./components/Hero";
 import { Projects } from "./components/Projects";
 import { Contact } from "./components/Contact";
+import { useEffect, useState } from "react";
 
 function App() {
+  const [isLoaded, setIsLoaded] = useState(false)
+
+  useEffect(() => {
+    setIsLoaded(true);
+  }, [])
+
   return ( 
-  <> 
+  <div className={`app ${isLoaded ? "loaded" : ""}`}> 
     <Navbar />
     <Hero />
     <Projects />
@@ -15,7 +22,7 @@ function App() {
     <>
       <p> &copy; 2025 Ben Ellis. All rights reserved.</p>
     </>
-  </>
+  </div>
   );
 }
 
